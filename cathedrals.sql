153c700850b1bf226867ab335fe8149442d37cc2
Vasilev Valery <vvi.20030613@gmail.com>
2025-12-26 01:03:03 +0900

    cathedrals

---
+++ cathedrals.sql
@@ -0,0 +1,22 @@
+		CREATE TABLE cathedrals (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            chief VARCHAR(255)
+        );
+
+        ALTER TABLE groups
+        ADD COLUMN cathedral_id INTEGER;
+
+        INSERT INTO cathedrals (name, chief) VALUES
+        ('Кафедра информационных технологий', 'Васильева Наталья Васильевна'),
+        ('Математическая экономика и прикладная информатика', 'Матвеева Нюргуяна Николаевна');
+
+        SELECT * FROM cathedrals;
+
+        UPDATE groups
+		SET cathedral_id = 1
+		WHERE id IN (1, 2, 3);
+
+		UPDATE groups
+		SET cathedral_id = 2
+		WHERE id = 4;
